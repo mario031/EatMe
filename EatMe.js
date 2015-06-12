ad1630ea88599a2ac1592abd00e9595d90329552
mario031 <reimariodayo@gmail.com>
2015-06-13 01:49:25 +0900

    EatMe最初のコミット

---
+++ EatMe.js
@@ -0,0 +1,68 @@
+var SensorTag = require('sensortag');
+
+//function onDiscover(sensorTag){
+
+//}
+
+SensorTag.discoverAll(function(sensorTag) {
+	sensorTag.connect(function() {
+
+	    sensorTag.discoverServicesAndCharacteristics(function() {
+
+	        sensorTag.enableAccelerometer(function() {
+	
+	            sensorTag.setAccelerometerPeriod(5, function() {
+	            })
+
+	            sensorTag.on('accelerometerChange', function(x, y, z){
+
+		            function accChange() {
+			            console.log('accX', {accX: x});
+			            console.log('accY', {accY: y});
+			            console.log('accZ', {accZ: z});
+		            }; 
+		            accChange();
+	            })
+	            
+	            sensorTag.notifyAccelerometer(function() {
+                    console.log("> accelerometer is on");
+                });
+	        });
+
+	        sensorTag.enableIrTemperature(function() {
+
+	        	sensorTag.setIrTemperaturePeriod(5, function() {
+	        	})
+
+	        	sensorTag.on('irTemperatureChange', function(obj, amb){
+
+	        		function tempChange() {
+	        			console.log('obj', {obj: obj});
+	        			console.log('amb', {amb: amb});
+	        		};
+	        		tempChange();
+	        	})
+
+	        	sensorTag.notifyIrTemperature(function() {
+	        		console.log("> temperature is on");
+	        	});
+	        });
+
+	        /*sensorTag.on('simpleKeyChange', function(left, right) {
+
+	        	function buttons() {
+	        		var left = SensorTag.accChange();
+	        	    var right = SensorTag.tempChange();
+
+	        	    //callback(left, right);
+	            };
+	            buttons();	        	
+	        })
+
+	        sensorTag.notifySimpleKey(function() {
+	        	console.log("> simpleKey is on");
+	        });*/
+
+	    });
+	});
+});
