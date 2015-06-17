125c58cf15d70ccf5c4007cf916ae4af5f897dfc
mario031 <reimariodayo@gmail.com>
2015-06-17 13:34:11 +0900

    ブラウザ表示しちゃうよん

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ var eat = require('./EatMe');
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("x" + eat.accX);
-  res.write("y" + eat.accY);
-  res.write("z" + eat.accZ);
-  res.write("obj" + eat.obj);
-  res.write("amb" + eat.amb);
+  res.write("SensorTag1" + "\nx:" + eat.accX_1+ "\ny:" + eat.accY_1 + "\nz:" + eat.accZ_1 + "\nobj:" + eat.obj_1 + "\namb:" + eat.amb_1 + '\n');
+  res.write("\nSensorTag2" + "\nx:" + eat.accX_2+ "\ny:" + eat.accY_2 + "\nz:" + eat.accZ_2 + "\nobj:" + eat.obj_2 + "\namb:" + eat.amb_2);
   res.end();
 }).listen(1337, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:1337/');
