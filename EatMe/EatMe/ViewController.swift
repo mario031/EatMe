4c1b627342bafbc35c3a6f84adb8182b23e4b8eb
mario031 <reimariodayo@gmail.com>
2015-06-26 08:43:44 +0900

    iPhoneで表示したい

---
+++ EatMe/EatMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EatMe
+//
+//  Created by 石川伶 on 2015/06/26.
+//  Copyright (c) 2015年 石川伶. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
