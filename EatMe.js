1bb34bf63e95615e5db34b3268a211b17bd3b098
mario031 <reimariodayo@gmail.com>
2015-06-30 23:30:55 +0900

    postするnodeだよ

--- EatMe.js
+++ EatMe.js
@@ -1,19 +1,7 @@
 var SensorTag = require('sensortag');
-var mongoose = require('mongoose');
+var http = require('request');
 
-
-
-var EatSchema = new mongoose.Schema({
-  id1:{type:String},
-  id2:{type:String},
-  date1:{type:String},
-  date2:{type:String}
-});
-mongoose.model('Eat', EatSchema);
-
-// mongodbに接続
-var eatme = mongoose.connect('mongodb://localhost/eatme');
-var Eat = eatme.model('Eat');
+var baseUrl = 'http://life-cloud.ht.sfc.keio.ac.jp/~mario/response.php';
 
 
 
@@ -46,14 +34,17 @@ SensorTag.discoverAll(function(sensorTag) {
 	    				
 	    				function accChange() {
 	    					if(x < -2 || 2 < x){
-	    						var eat = new Eat();
-		            		    eat.id1 = '5c:31:3e:00:00:bf:fc:15';
-		            		    eat.date1 = accData;
-		            		    eat.save(function(err){
-	    						   if(err){
-	    							console.log('save error!');
-	    						   }
-	    					    });
+	    						request.post(
+	    							{url: baseUrl, json:true},
+	    							{ form: { name:'mario', id:'5c:31:3e:00:00:bf:fc:15', date: Date.noe()}},
+	    							function(err,res,body){
+	    								if(!err && res.statusCode == 200){
+	    									var parse_body = qs.parse(body);
+	    								console.log(body);
+	    							}else{
+	    								console.log(body);
+	    							}
+	    							});
 	    					}    					
 		                };
 
@@ -101,18 +92,11 @@ SensorTag.discoverAll(function(sensorTag) {
 	    					var milisecond = date.getMilliseconds();
 	    					var nowTime = hour + ":" + minute + ":" + second + ":" + milisecond;
 	    					var nowDate = year + "-" + month + "-" + day;
-	    					var accData =  nowDate + nowTime;
+	    					var accData =  nowDate + ' ' + nowTime;
 	    				
 	    				function accChange() {
 		            	    if(x < -2 || 2 < x){
-		            		    var eat = new Eat();
-		            		    eat.id2 = '5c:31:3e:00:00:bf:e8:7a';
-		            		    eat.date2 = accData;
-		            		    eat.save(function(err){
-	    						    if(err){
-	    							    console.log('save error!');
-	    						    }
-	    					    });
+		            		    
 	    					}    
 		                };
 
