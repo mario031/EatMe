fee0e5d4420b4a49039ac8c110ce1417198451bd
mario031 <reimariodayo@gmail.com>
2015-07-01 02:31:37 +0900

    mongoからfindできたんご

---
+++ find.php
@@ -0,0 +1,13 @@
+<?php
+// DBへ接続
+$mongo = new Mongo();
+// データベースを指定
+$db = $mongo->selectDB("eatme");
+// コレクションを指定1
+$col = $db->selectCollection("eatmes");
+// コレクションのドキュメントを全件取得
+$cursor = $col->find();
+// 表示
+foreach ($cursor as $id =>$obj) {
+	var_dump($obj);
+}
