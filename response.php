64d0cbe6940f9f35c01c39e65b3e3c4053e41923
mario031 <reimariodayo@gmail.com>
2015-07-01 02:31:24 +0900

    mongoにsaveできたんご

--- response.php
+++ response.php
@@ -1,5 +1,16 @@
 <?php
 
+$post = $_POST;
+// DBへ接続
+$mongo = new Mongo();
 
-$ post = $_POST;
-var_dump($ post);
+// データベースを指定
+$db = $mongo->selectDB("eatme");
+
+// コレクションを指定1
+$col = $db->selectCollection("eatmes");
+
+$col->save($post);
+//var_dump($post);
+
+include("find.php");
