1984d746f45ec5b3d4f89ab609cd219b588c1d36
mario031 <reimariodayo@gmail.com>
2015-07-02 18:11:19 +0900

    iPhoneに表示

--- EatMe/EatMe/ViewController.swift
+++ EatMe/EatMe/ViewController.swift
@@ -13,13 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        //Construct url object via string
+        var url = NSURL(string: "http://life-cloud.ht.sfc.keio.ac.jp/~mario/find.php")
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+        var req = NSURLRequest(URL: url!)
+        
+        //NSURLSessionDownloadTask is retured from session.dataTaskWithRequest
+        var task = session.dataTaskWithRequest(req, completionHandler: {
+            (data, resp, err) in
+            println(resp.URL!)
+            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+        })
+        task.resume()
+
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
-
-
 }
 
