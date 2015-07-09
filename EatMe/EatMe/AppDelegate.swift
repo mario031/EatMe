bb75d7e02b4a478622354262da3ee4289d21d1d1
mario031 <reimariodayo@gmail.com>
2015-07-10 03:38:06 +0900

    iPhoneに表示させたど

--- EatMe/EatMe/AppDelegate.swift
+++ EatMe/EatMe/AppDelegate.swift
@@ -13,35 +13,72 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+//    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+//        
+//    let settings = UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert, categories: nil)
+//    UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+//    UIApplication.sharedApplication().setMinimumBackgroundFetchInterval(UIApplicationBackgroundFetchIntervalMinimum)
+//        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: UIUserNotificationType.Sound | UIUserNotificationType.Alert | UIUserNotificationType.Badge, categories: nil))
+//
+//    return true;
+//    }
+//    
+//    func application(application: UIApplication, performFetchWithCompletionHandler completionHandler: (UIBackgroundFetchResult) -> Void) {
+//        println("Complete");
+//        completionHandler(UIBackgroundFetchResult.NewData)
+//        
+//        getData();
+//    }
+//    
+//    func getData() -> Void{
+//        /// 通信先のURLを生成.
+//        let myUrl:NSURL = NSURL(string: "http://life-cloud.ht.sfc.keio.ac.jp/~mario/find.php")!
+//        // セッションの生成.
+//        let mySession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+//        // 通信のタスクを生成.
+//        let myTask = mySession.dataTaskWithURL(myUrl, completionHandler: {
+//            (data, response, err) in
+//            var localNotification:UILocalNotification = UILocalNotification()
+//            localNotification.alertAction = "OK"
+//            localNotification.alertBody = "動いてないよー"
+//            localNotification.fireDate = NSDate(timeIntervalSinceNow: 1)
+//            UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+//            var eatme = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as! NSDictionary;
+//            var eatme_name: [NSDictionary]=[];
+//            eatme_name = eatme["name"] as! [NSDictionary];
+//
+//            
+//            // 帰ってきたデータを文字列に変換.
+//            var myData:NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
+//            
+//            var localNotification:UILocalNotification = UILocalNotification()
+//            localNotification.timeZone = NSTimeZone.defaultTimeZone()
+//            localNotification.alertAction = "OK"
+//            localNotification.alertBody = "動いてないよー"
+//            UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+//            
+//            var alert = UIAlertView();
+//            alert.title = "受け取りました";
+//            alert.message = localNotification.alertBody;
+//            alert.addButtonWithTitle(localNotification.alertAction!);
+//            alert.show();
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        
-        return true
-    }
-
+//        })
+////         タスクの実行
+//        myTask.resume()
+//      
+//    }
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
--- EatMe/EatMe/ViewController.swift
+++ EatMe/EatMe/ViewController.swift
@@ -14,14 +14,24 @@ class ViewController: UIViewController,NSURLSessionDelegate,NSURLSessionDataDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        myTextView = UITextView(frame:CGRectMake(10, 50, self.view.frame.size.width, self.view.frame.size.height))
+        // 背景を灰色に設定する.
+        self.view.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)
         
-        myTextView.font = UIFont.systemFontOfSize(CGFloat(20))
+        myTextView = UITextView(frame:CGRectMake(10, 50, self.view.frame.size.width - 20, self.view.frame.size.height - 70))
+        myTextView.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 1, alpha: 1.0)
+        myTextView.layer.masksToBounds = true
+        myTextView.layer.cornerRadius = 20.0
+        myTextView.layer.borderWidth = 1
+        myTextView.layer.borderColor = UIColor.blackColor().CGColor
+        myTextView.font = UIFont.systemFontOfSize(CGFloat(25))
         myTextView.textColor = UIColor.blackColor()
         myTextView.textAlignment = NSTextAlignment.Left
+        myTextView.dataDetectorTypes = UIDataDetectorTypes.All
+        myTextView.layer.shadowOpacity = 0.5
+        myTextView.editable = false
         self.view.addSubview(myTextView)
+        
         
         NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector:Selector("update"), userInfo: nil, repeats: false)
         
@@ -35,20 +45,23 @@ class ViewController: UIViewController,NSURLSessionDelegate,NSURLSessionDataDele
         let myTask:NSURLSessionDataTask = mySession.dataTaskWithURL(myUrl)
         // タスクの実行.
         myTask.resume()
+        
     }
         /*
         通信が終了したときに呼び出されるデリゲート.
         */
         func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData) {
             println("NSURLSessionDataTask")
-            
+        
             // 帰ってきたデータを文字列に変換.
             var myData:NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
             
             // バックグラウンドだとUIの処理が出来ないので、メインスレッドでUIの処理を行わせる.
             dispatch_async(dispatch_get_main_queue(), {
                 self.myTextView.text = myData as String
+//                self.showNotification1()
             })
+        
         }
         /*
         バックグラウンドからフォアグラウンドの復帰時に呼び出されるデリゲート.
@@ -57,7 +70,7 @@ class ViewController: UIViewController,NSURLSessionDelegate,NSURLSessionDataDele
             println("URLSessionDidFinishEventsForBackgroundURLSession")
         }
     
-    //一定時間毎に更新
+//    一定時間毎に更新
     func update(){
         viewDidLoad()
     }
@@ -66,6 +79,7 @@ class ViewController: UIViewController,NSURLSessionDelegate,NSURLSessionDataDele
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 }
 
 
