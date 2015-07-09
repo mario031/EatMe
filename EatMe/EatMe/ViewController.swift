e38d24ebd14657cea9de8b6f42857716d842e486
mario031 <reimariodayo@gmail.com>
2015-07-09 18:36:05 +0900

    iPhoneに表示

--- EatMe/EatMe/ViewController.swift
+++ EatMe/EatMe/ViewController.swift
@@ -9,45 +9,59 @@
 import UIKit
 import SwiftyJSON
 
-class ViewController: UIViewController {
-    private var myScrollView: UIScrollView!
+class ViewController: UIViewController,NSURLSessionDelegate,NSURLSessionDataDelegate{
+    private var myTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        myScrollView = UIScrollView()
-        myScrollView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
+        myTextView = UITextView(frame:CGRectMake(10, 50, self.view.frame.size.width, self.view.frame.size.height))
         
-        let myAppFrameSize: CGSize = UIScreen.mainScreen().applicationFrame.size
-        let mylabel: UILabel = UILabel(frame: CGRectMake(10,40,myAppFrameSize.width,myAppFrameSize.height*2))
+        myTextView.font = UIFont.systemFontOfSize(CGFloat(20))
+        myTextView.textColor = UIColor.blackColor()
+        myTextView.textAlignment = NSTextAlignment.Left
+        self.view.addSubview(myTextView)
         
-        //NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "update", userInfo: nil, repeats: true)
+        NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector:Selector("update"), userInfo: nil, repeats: false)
         
-        var url = NSURL(string: "http://life-cloud.ht.sfc.keio.ac.jp/~mario/find.php")
-        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
-        let session = NSURLSession(configuration: config)
-        var req = NSURLRequest(URL: url!)
-        
-        //NSURLSessionDownloadTask is retured from session.dataTaskWithRequest
-        var task = session.dataTaskWithRequest(req, completionHandler: {
-            (data, resp, err) in
-            var value: String = NSString(data:data, encoding:NSUTF8StringEncoding)! as String
-            println(value)
-//            mylabel.text = value
-//            mylabel.numberOfLines = 0
-//            mylabel.sizeToFit();
-        })
-        task.resume()
-
-//        func update(){
-//            get.resume()
-//        }
-        
-        myScrollView.addSubview(mylabel)
-        myScrollView.contentSize = CGSizeMake(mylabel.frame.size.width, mylabel.frame.size.height)
-        self.view.addSubview(myScrollView)
+        // 通信用のConfigを生成.
+        let myConfig:NSURLSessionConfiguration = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier("backgroundTask")
+        // Sessionを生成.
+        var mySession:NSURLSession = NSURLSession(configuration: myConfig, delegate: self, delegateQueue: nil)
+        // 通信先のURLを生成.
+        let myUrl:NSURL = NSURL(string: "http://life-cloud.ht.sfc.keio.ac.jp/~mario/find.php")!
+        // タスクの生成.
+        let myTask:NSURLSessionDataTask = mySession.dataTaskWithURL(myUrl)
+        // タスクの実行.
+        myTask.resume()
     }
+        /*
+        通信が終了したときに呼び出されるデリゲート.
+        */
+        func URLSession(session: NSURLSession, dataTask: NSURLSessionDataTask, didReceiveData data: NSData) {
+            println("NSURLSessionDataTask")
+            
+            // 帰ってきたデータを文字列に変換.
+            var myData:NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
+            
+            // バックグラウンドだとUIの処理が出来ないので、メインスレッドでUIの処理を行わせる.
+            dispatch_async(dispatch_get_main_queue(), {
+                self.myTextView.text = myData as String
+            })
+        }
+        /*
+        バックグラウンドからフォアグラウンドの復帰時に呼び出されるデリゲート.
+        */
+        func URLSessionDidFinishEventsForBackgroundURLSession(session: NSURLSession) {
+            println("URLSessionDidFinishEventsForBackgroundURLSession")
+        }
+    
+    //一定時間毎に更新
+    func update(){
+        viewDidLoad()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
